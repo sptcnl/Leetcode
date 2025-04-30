735f9fc3983b8263b89c7b293b561484891be00e
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-04-30 10:43:18 +0900

    Time: 0 ms (100%), Space: 19.1 MB (8.75%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,9 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i=0 
-        for j in range(1,len(nums)):
-            if nums[i]!=nums[j]: # 중복된 수가 아니라면
-                i+=1  # i를 옮기고
-                nums[i]=nums[j] # nums의 i번째 수를 nums의 j번째 수로 바꾸기
-        
+        i=0
+        for j in range(1, len(nums)):
+            if nums[i]!=nums[j]:
+                i+=1
+                nums[i]=nums[j]
         return i+1
