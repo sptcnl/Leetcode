c83e62a1fe1fabe6f655914a31a05f5843c31e16
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-09 11:29:23 +0900

    Time: 7 ms (39.98%), Space: 18 MB (25.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -12,13 +12,13 @@ class Solution:
                 if num == '.':
                     continue
                 num = int(num)
-                box_idx = (r // 3) * 3 + (c // 3)
+                boxes_idx = ((r // 3) * 3) + (c // 3)
 
-                if rows[r][num] or cols[c][num] or boxes[box_idx][num]:
+                if rows[r][num] or cols[c][num] or boxes[boxes_idx][num]:
                     return False
 
                 rows[r][num] += 1
                 cols[c][num] += 1
-                boxes[box_idx][num] += 1
+                boxes[boxes_idx][num] += 1
 
         return True
