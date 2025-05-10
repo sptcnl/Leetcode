47520e47e001c060dda8915ea08cf072e1ee9b5e
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-10 23:23:39 +0900

    Time: 3 ms (65.99%), Space: 19.5 MB (77.73%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         n = nums[0]
-        for i in range(1,len(nums)):
+        for i in range(1, len(nums)):
             n ^= nums[i]
         return n
