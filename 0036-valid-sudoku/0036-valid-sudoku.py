83d5d1899fcdcc14cf6bc3292956281575c7e7cd
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-17 00:00:12 +0900

    Time: 3 ms (83.57%), Space: 17.7 MB (69.86%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,24 +1,31 @@
 from collections import defaultdict
+from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [defaultdict(int) for _ in range(9)]
-        cols = [defaultdict(int) for _ in range(9)]
-        boxes = [defaultdict(int) for _ in range(9)]
+        # 각각 열, 행, 3x3 박스를 저장할 defaultdict
+        cols = defaultdict(list)  # 열은 j 기준
+        rows = defaultdict(list)  # 행은 i 기준
+        boxes = defaultdict(list)  # 박스는 (i//3, j//3) 조합 기준
 
-        for r in range(9):
-            for c in range(9):
-                num = board[r][c]
-                if num == '.':
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num == ".":
                     continue
-                num = int(num)
-                boxes_idx = ((r // 3) * 3) + (c // 3)
-
-                if rows[r][num] or cols[c][num] or boxes[boxes_idx][num]:
+                
+                # 3x3 박스 인덱스 계산
+                box_index = (i // 3) * 3 + (j // 3)
+                
+                # 중복 체크
+                if (num in rows[i] or 
+                    num in cols[j] or 
+                    num in boxes[box_index]):
                     return False
-
-                rows[r][num] += 1
-                cols[c][num] += 1
-                boxes[boxes_idx][num] += 1
-
+                
+                # 값 저장
+                rows[i].append(num)
+                cols[j].append(num)
+                boxes[box_index].append(num)
+        
         return True
