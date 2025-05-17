401cca154628d125d5195ca063dfa1a5dcde0620
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-18 00:33:05 +0900

    Time: 0 ms (100%), Space: 17.7 MB (77.04%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -3,22 +3,26 @@ import re
 class Solution:
     def myAtoi(self, s: str) -> int:
         s = s.lstrip()
-        if not s:
-            return 0
-        
         sign = 1
-        if s[0] in ('+', '-'):
-            sign = -1 if s[0] == '-' else 1
+        if s and (s[0] == "-" or s[0] == "+"):
+            sign = -1 if s[0] == "-" else 1
             s = s[1:]
-        
-        num_str = []
-        for c in s:
-            if not c.isdigit():
+
+        result = 0
+        for w in s:
+            if w.isdigit():
+                result = result * 10 + int(w)
+            else:
                 break
-            num_str.append(c)
-        
-        if not num_str:
-            return 0
-        
-        num = int(''.join(num_str)) * sign
-        return max(min(num, 2**31-1), -2**31)
+
+        result *= sign
+
+        min_int = -2 ** 31
+        max_int = (2 ** 31) - 1
+
+        if result < min_int:
+            return min_int
+        elif result > max_int:
+            return max_int
+        else:
+            return result
