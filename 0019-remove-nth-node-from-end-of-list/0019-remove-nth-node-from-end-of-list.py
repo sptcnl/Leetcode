481b7cfd46d9a71bc0b9231de8ccd953592dfabd
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-21 14:03:48 +0900

    Time: 2 ms (7.17%), Space: 17.7 MB (54.81%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -9,16 +9,12 @@ class Solution:
         first = dummy
         second = dummy
         
-        # first를 n+1칸 앞으로 이동
         for _ in range(n + 1):
             first = first.next
         
-        # first가 끝에 도달할 때까지 두 포인터 같이 이동
         while first:
             first = first.next
             second = second.next
         
-        # second.next가 삭제할 노드
         second.next = second.next.next
-        
         return dummy.next
