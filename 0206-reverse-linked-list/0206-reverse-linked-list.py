9df1e93d8d2141afdfca3a335ecb8cdd5a78e4da
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-22 14:46:28 +0900

    Time: 0 ms (100%), Space: 18.6 MB (79.56%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -6,10 +6,8 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         node, prev = head, None
-        
+
         while node:
-            # node.next를 이전 prev 리스트로 계속 연결하면서 끝날 때까지 반복
             next, node.next = node.next, prev
             prev, node = node, next
-            
         return prev
