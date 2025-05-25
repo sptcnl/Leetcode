a73e3126a805e0794072e92bd727fb7400fd107d
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-25 16:02:55 +0900

    Time: 7 ms (71.61%), Space: 17.8 MB (83.81%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,16 +1,15 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
         result = "1"
-        for _ in range(n-1):
+        for _ in range(n - 1):
             temp = ""
             i = 0
             while i < len(result):
                 count = 1
-                while (i + 1 < len(result)) and (result[i] == result[i + 1]):
+                while i + 1 < len(result) and result[i] == result[i + 1]:
                     i += 1
                     count += 1
                 temp += str(count) + result[i]
                 i += 1
-            print("temp print: ", temp)
             result = temp
         return result
