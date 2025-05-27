1588aa4cff5235b621ea1e1b1181c6410d38072e
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-27 18:06:08 +0900

    Time: 0 ms (100%), Space: 20 MB (8.03%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -9,10 +9,7 @@ class Solution:
         def validate(node, min_val=float('-inf'), max_val=float('inf')):
             if not node:
                 return True
-            if not (min_val < node.val < max_val):
+            elif not (min_val < node.val < max_val):
                 return False
-            return (validate(node.left, min_val, node.val) and 
-                    validate(node.right, node.val, max_val))
-        
+            return validate(node.left, min_val, node.val) and validate(node.right, node.val, max_val)
         return validate(root)
-        
