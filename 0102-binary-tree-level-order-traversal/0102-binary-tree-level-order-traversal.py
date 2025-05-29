adde2f710c252ba100bf77c741837e8676b459a5
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-29 15:53:32 +0900

    Time: 3 ms (20.68%), Space: 18.5 MB (39.09%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -10,13 +10,10 @@ class Solution:
         
         def dfs(node, level):
             if not node:
-                return
-            # 현재 level에 해당하는 리스트가 없으면 추가
+                return None
             if len(result) == level:
-                result.append([])
-            # 현재 노드의 값을 해당 level 리스트에 추가
+                result.append(list())
             result[level].append(node.val)
-            # 왼쪽, 오른쪽 자식 노드로 재귀 호출
             dfs(node.left, level + 1)
             dfs(node.right, level + 1)
 
