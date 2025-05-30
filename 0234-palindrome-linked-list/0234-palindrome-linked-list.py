b51f0100e29c336cb3fbbc0a09e708d4a44db292
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-31 00:46:59 +0900

    Time: 23 ms (82.85%), Space: 39.3 MB (44.21%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,19 +5,17 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        length_of_list = 0
-        stack = []
-        node = head
-        node2 = head
-        while node:
-            stack.append(node.val)
-            length_of_list += 1
-            node = node.next
-        itr_range = length_of_list // 2 if length_of_list % 2 == 0 else (length_of_list - 1) // 2
-
+        len_of_list = 0
+        stack  = []
+        cur = head
+        while cur:
+            stack.append(cur.val)
+            len_of_list += 1
+            cur = cur.next
+        itr_range = len_of_list // 2
+        cur = head
         for i in range(itr_range):
-            if node2.val == stack.pop():
-                node2 = node2.next
-            else:
+            if cur.val != stack.pop():
                 return False
+            cur = cur.next
         return True
