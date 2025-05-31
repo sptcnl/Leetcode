bdf7716ef6afe097a8802a5174788881932bb89e
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-05-31 22:41:55 +0900

    Time: 0 ms (100%), Space: 17.8 MB (71.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,11 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m - 1  # nums1의 마지막 유효 원소 인덱스
-        j = n - 1  # nums2의 마지막 원소 인덱스
-        k = m + n - 1  # 병합된 배열의 마지막 위치
-        
-        # 역방향으로 큰 값부터 채우기
+        i = m - 1
+        j = n - 1
+        k = m + n -1
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -16,9 +14,8 @@ class Solution:
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
-        
-        # nums2에 남은 원소가 있는 경우 처리
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
             k -= 1
+        return nums1
