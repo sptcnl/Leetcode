c3b891756348e3c664ca608879ff6403381d0f70
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-06-02 00:09:07 +0900

    Time: 39 ms (44.89%), Space: 17.6 MB (68.95%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -6,9 +6,9 @@ class Solution:
         start = 1
         end = n
         while start <= end:
-            middle = (start + end) // 2
-            if isBadVersion(middle):
-                end = middle - 1
+            mid = (start + end) // 2
+            if isBadVersion(mid):
+                end = mid - 1
             else:
-                start = middle + 1
+                start = mid + 1
         return start
