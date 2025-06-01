81d339387a5c96ae9a00c09007eb8e7382c305c7
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-06-02 01:39:04 +0900

    Time: 0 ms (100%), Space: 17.8 MB (30.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,9 @@
+import math
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-
-        dp = [0] * (n + 1)
-        dp[1] = 1
-        dp[2] = 2
-        
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        
-        return dp[n]
+        sqrt5 = math.sqrt(5)
+        phi = (1 + sqrt5) / 2
+        psi = (1 - sqrt5) / 2
+        fib_n = (phi**(n+1) - psi**(n+1)) / sqrt5
+        return int(round(fib_n))
