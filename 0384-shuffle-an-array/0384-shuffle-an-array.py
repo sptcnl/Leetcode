37e4cac6124771d84ee9e30bf96a188d7870d6c8
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-06-05 16:30:39 +0900

    Time: 45 ms (10.41%), Space: 21.8 MB (80.67%) - LeetHub

---
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -0,0 +1,23 @@
+from random import shuffle
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def reset(self) -> List[int]:
+        return self.nums
+
+    def shuffle(self) -> List[int]:
+        array = self.nums[:]
+        n = len(array)
+        for i in range(n):
+            j = random.randint(i, n - 1)  # i 이상 n-1 이하 중 무작위 인덱스
+            array[i], array[j] = array[j], array[i]  # 두 원소를 swap
+        return array
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
