8e1e46b6fcf1b40317a28d0c21086a80f15a7571
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-06-07 00:18:01 +0900

    Time: 0 ms (100%), Space: 17.7 MB (54.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,11 +4,8 @@ class Solution:
             return 0
         elif len(nums) == 1:
             return nums[0]
-        
         prev = nums[0]
-        cur_num = max(nums[0], nums[1])
-
+        curr = max(nums[0], nums[1])
         for num in nums[2:]:
-            prev, cur_num = cur_num, max(cur_num, prev + num)
-
-        return cur_num
+            prev, curr = curr, max(curr, prev + num)
+        return curr
