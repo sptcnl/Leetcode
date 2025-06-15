003d7683813a55c2a852e9048f1a3e06789cd16d
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-06-15 23:19:54 +0900

    Time: 0 ms (100%), Space: 18 MB (16.27%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -5,8 +5,6 @@ class Solution:
             row = [1] * (i+1)
             if i > 1:
                 for j in range(1, i):
-                    print(i-1, j-1, i-1, j)
                     row[j] = result[i-1][j-1] + result[i-1][j]
-            print(i, row)
             result.append(row)
         return result
