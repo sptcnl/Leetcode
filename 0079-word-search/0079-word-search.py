a53939ec65a5abfdcfb20c65914deb7230e3e47a
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-07-07 17:13:41 +0900

    Time: 3156 ms (82.51%), Space: 17.7 MB (95.93%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -4,25 +4,21 @@ class Solution:
 
         def dfs(r, c, idx):
             if idx == len(word):
-                return True  # 모든 글자를 찾음
-            if r < 0 or r >= rows or c < 0 or c >= cols:
-                return False  # 범위 밖
+                return True
+            if r < 0 or c < 0 or r >= rows or c >= cols:
+                return False
             if board[r][c] != word[idx]:
-                return False  # 글자가 다름
-
+                return False
             temp = board[r][c]
-            board[r][c] = '#'  # 방문 표시
-
-            # 상, 하, 좌, 우 네 방향 탐색
-            found = (
-                dfs(r + 1, c, idx + 1) or
-                dfs(r - 1, c, idx + 1) or
-                dfs(r, c + 1, idx + 1) or
-                dfs(r, c - 1, idx + 1)
+            board[r][c] = '#'
+            result = (
+                dfs(r+1, c, idx+1) or
+                dfs(r-1, c, idx+1) or
+                dfs(r, c+1, idx+1) or
+                dfs(r, c-1, idx+1)
             )
-
-            board[r][c] = temp  # 원상복구
-            return found
+            board[r][c] = temp
+            return result
 
         for i in range(rows):
             for j in range(cols):
