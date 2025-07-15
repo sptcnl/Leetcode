295b55cebac4b3b4c7929af9659373386b29111b
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-07-15 16:12:46 +0900

    Time: 24 ms (61.11%), Space: 18.8 MB (14.02%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,8 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        max_reach = 0
-        for i in range(len(nums)):
-            if i > max_reach:
-                return False  # 현재 위치까지 못 도달한다면 실패
-            max_reach = max(max_reach, i + nums[i])
-        return True
+        if 0 in nums[:-1]:
+            max_reach = 0
+            for i in range(len(nums)):
+                if i > max_reach:
+                    return False  # 현재 위치까지 못 도달한다면 실패
+                max_reach = max(max_reach, i + nums[i])
+            return True
+        else:
+            return True
