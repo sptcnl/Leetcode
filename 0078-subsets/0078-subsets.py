cb15c7c45b01e3c5f8f2a197cc8a3bba9328320e
sptcnl <119477585+sptcnl@users.noreply.github.com>
2025-08-10 23:53:47 +0900

    Time: 0 ms (100%), Space: 17.8 MB (65.3%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,11 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
+        # 부분집합을 생성하는 재귀 함수
         def dfs(idx, path):
-            result.append(path[:])
+            result.append(path[:])  # 현재 경로(부분집합)를 결과에 복사해서 추가
+            # idx부터 시작해서 각 요소에 대해 부분집합 만들기 시도
             for i in range(idx, len(nums)):
-                path.append(nums[i])
-                dfs(i+1, path)
-                path.pop()
+                path.append(nums[i])    # 현재 숫자를 부분집합에 포함
+                dfs(i+1, path)  # 다음 인덱스로 재귀 호출
+                path.pop()  # 재귀 호출 이후, 현재 숫자를 빼주어(백트래킹) 다른 경로 탐색
         dfs(0, [])
         return result
